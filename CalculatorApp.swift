ef734296ee8ab0b65b0b931a2adde03a06f5f106
Kiruthika Marikumaran <kiruthika.star14@gmail.com>
2025-03-03 17:52:44 -0800

    calculator ios

---
+++ CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Kiruthika Marikumaran on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorLogic.h
@@ -0,0 +1,15 @@
+// CalculatorLogic.h
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorLogic : NSObject
+
+- (double)add:(double)a with:(double)b;
+- (double)subtract:(double)a with:(double)b;
+- (double)multiply:(double)a with:(double)b;
+- (double)divide:(double)a with:(double)b;
+- (double)sine:(double)x;
+- (double)cosine:(double)x;
+- (double)tangent:(double)x;
+
+@end
